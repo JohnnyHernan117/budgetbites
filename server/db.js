686a9f47c4c7649edee37399eb7194f140433ae5
JohnnyHernan117 <johnnyhernan117@gmail.com>
2026-04-08 00:17:44 -0700

    use render internal database url

--- server/db.js
+++ server/db.js
@@ -6,10 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL
 });
 
 export default pool;
